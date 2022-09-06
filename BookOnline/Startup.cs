657674cc6e8f0a488ad307a45e5a00aa36cf1b49
PC <PC@DESKTOP-592MVUG>
2022-09-06 18:42:55 +0700

    Add image review project

--- BookOnline/Startup.cs
+++ BookOnline/Startup.cs
@@ -66,7 +66,7 @@ namespace BookOnline
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
